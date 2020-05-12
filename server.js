993efe4bcc2238716b2b1d906f5522995de16524
kishan pandey <41380677+kishanrocks7@users.noreply.github.com>
2020-05-12 10:49:13 +0530

    setting up socket.io

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express =require('express');
 const path=require('path');
+const http=require('http');
 const app=express();
-
+const socketio=require('socket.io');
+const server=http.createServer(app);
+const io=socketio(server);
 //set static folder
 
-app.use(express.static(path.joi));
+app.use(express.static(path.join(__dirname,'public')));
+
+//run when aclien connects
+
+io.on('connection',socket=> {
+    console.log('new ws connection.....');
+})
 
 const PORT = 3000 || process.env.PORT;
 
