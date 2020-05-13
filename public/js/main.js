3346cba95214c64340b23a26b8db2eaf799221ca
kishan pandey <41380677+kishanrocks7@users.noreply.github.com>
2020-05-13 13:42:10 +0530

    chatting function working

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,4 @@
 const chatForm = document.getElementById('chat-form');
-const chatMessages =document.querySelector('.chat-messages');
-const socket = io();const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
 const roomName = document.getElementById('room-name');
 const userList = document.getElementById('users');
@@ -66,37 +64,4 @@ function outputUsers(users) {
   userList.innerHTML = `
     ${users.map(user => `<li>${user.username}</li>`).join('')}
   `;
-}
-
-
-socket.on('message',message=>{
-  console.log(message);
-
-  //scroll down
-  chatMessages.scrollTop = chatMessages.scrollHeight;
-});
-
-//message submit
-chatForm.addEventListener('submit' , (e) =>{
-  e.preventDefault();
-
-
-  //get message text
-  const msg =e.target.elements.msg.value;
-//emitting a message to server
-  socket.emit('chatMessage',msg);
-  e.target.element.msg.value="";
-  e.target.element.msg.focus();
-});
-
-//output mesaage to DOM
-
-function outputMessage(message) {
-  const div = document.createElement('div');
-  div.classList.add('message');
-  div.innerHTML = ` <p class="meta">brand<span> 9:12pm</span></p>
-  <p class="text">
-  ${message}
-  </p>`;
-  document.querySelector('.chat-message').appendChild(div);
 }
